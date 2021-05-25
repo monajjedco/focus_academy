478e9e9695d070d969cacabf3e15b3966bb336e5
unknown <ahmad.m.monajjed@gmail.com>
2021-05-25 22:43:10 +0400

    first commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\User;
+use App\question;
+use App\answer;
 
 /*
 |--------------------------------------------------------------------------
@@ -66,13 +68,13 @@ Route::post('/explore_questions', function (Request $request) {
         	if($question_type)
         	{
 	         $all_questions= DB::table('questions')->where('question_status',$question_type)
-	                                               ->where('customer_id',$user)
+	                                               ->where('customer_id',$user->id)
 	                                               ->orderby('id','desc')
 	                                               ->get() ;         		
         	}
        	    else
        	    {
-	         $all_questions= DB::table('questions')->where('customer_id',$user)
+	         $all_questions= DB::table('questions')->where('customer_id',$user->id)
 	                                               ->orderby('id','desc')
 	                                               ->get() ;         	    	
        	    }         
@@ -114,7 +116,7 @@ Route::post('/search_question', function (Request $request) {
     {
 	    if ($user->type=='customer') 
 	    {   
-         $all_questions= DB::table('questions')->where('customer_id',$user)
+         $all_questions= DB::table('questions')->where('customer_id',$user->id)
 	                                               ->wherein('customer_id',$customer)
 	                                               ->orderby('id','desc')
 	                                               ->get() ; 
@@ -150,6 +152,97 @@ Route::post('/change_question_status', function (Request $request) {
                             ->update(['question_status' => $new_status]);
 
 	      
+    $has_error="No Errors";
+    return response()->json([$has_error]);
+    }
+    else
+    {
+    $has_error="Errors";
+    return response()->json($has_error);
+    }
+
+});
+
+
+
+
+
+
+//////////////////  API  ///////////////////////////Add_question by customer 
+Route::post('/add_question', function (Request $request) {
+
+    $token= $request->token ;
+    $question_text= $request->question_text ;
+
+    $user = User::where('api_token', $token)->first();
+    if ($user) 
+    {
+
+    question::create(array(
+        'question_text' => $request->question_text, 
+        'question_status' => 'Not Answered', 
+        'customer_id' => $user->id, 
+    ));    
+      	    	      
+    $has_error="No Errors";
+    return response()->json([$has_error]);
+    }
+    else
+    {
+    $has_error="Errors";
+    return response()->json($has_error);
+    }
+
+});
+//////////////////  API  //////////Delete_question by customer If status Not Answered
+Route::post('/delete_question', function (Request $request) {
+
+    $token= $request->token ;
+    $question_id= $request->question_id ;
+    $chk= DB::table('questions')->where('id',$question_id)->value('question_status');
+    $user = User::where('api_token', $token)->first();
+    if ($user && $chk=='Not Answered') 
+    {
+
+    DB::table('questions')->where('id',$question_id)->delete(); 
+
+    $has_error="Done And No Errors";
+    return response()->json([$has_error]);
+    }
+    else
+    {
+    $has_error="Not Done Or Errors";
+    return response()->json($has_error);
+    }
+
+});
+//////////////////  API  ///////////Add_Answer For Question by customer Or support team
+Route::post('/add_answer', function (Request $request) {
+
+    $token= $request->token ;
+
+    $user = User::where('api_token', $token)->first();
+    if ($user) 
+    {
+	    if ($user->type=='support') 
+	    {
+		    answer::create(array(
+		        'answer_text' => $request->answer_text, 
+		        'question_id' => $request->question_id, 
+		        'customer_id' => 0, 
+		        'support_id' =>  $request->support_id, 
+		    ));  
+		}
+		else
+		{
+		    answer::create(array(
+		        'answer_text' => $request->answer_text, 
+		        'question_id' => $request->question_id, 
+		        'customer_id' => $request->customer_id, 
+		        'support_id' =>  0, 
+		    )); 
+		}  
+      	    	      
     $has_error="No Errors";
     return response()->json([$has_error]);
     }
